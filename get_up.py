1cb2f140216ea5af2045f77bb3f6933a42bf721e
ed4win <qingliangx@126.com>
2023-04-04 13:23:59 +0800

    Update get_up.py

--- get_up.py
+++ get_up.py
@@ -9,7 +9,7 @@ import openai
 from BingImageCreator import ImageGen
 
 # 14 for test 12 real get up
-GET_UP_ISSUE_NUMBER = 12
+GET_UP_ISSUE_NUMBER = 4
 GET_UP_MESSAGE_TEMPLATE = "今天的起床时间是--{get_up_time}.\r\n\r\n 起床啦，喝杯咖啡，背个单词，去跑步。\r\n\r\n 今天的一句诗:\r\n {sentence} \r\n  ![image]({link})"
 SENTENCE_API = "https://v1.jinrishici.com/all"
 DEFAULT_SENTENCE = "赏花归去马如飞\r\n去马如飞酒力微\r\n酒力微醒时已暮\r\n醒时已暮赏花归\r\n"
